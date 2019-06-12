f8d0ad006c51a6f9676d3f28a8ca7e1012e33044
davidplbll <davidplbll@gmail.com>
2019-06-12 01:05:54 -0500

    primer bubida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  searchInput=''
+  image=""
+  name=""
+  file
+  picture="https://medicina.uv.cl/images/banco_de_fotos/especialidades-medicas.jpg"
+  constructor(private modalService: NgbModal){}
+  search(){
+
+  }
+  openLg(content) {
+    this.name=""
+    this.file=null;
+    this.picture="https://medicina.uv.cl/images/banco_de_fotos/especialidades-medicas.jpg"
+    this.modalService.open(content, { centered:true,size: 'lg' });
+  }
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let self = this;
+      self.file=fileList[0]
+      let fileReader: any = new FileReader();
+      fileReader.onload = (e) => {
+        self["picture"] = String(fileReader.result);
+      }
+      fileReader.readAsDataURL(fileList[0]);
+    }
+  }
 }
